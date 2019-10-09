f397d20469b54249e144edbea89e562bb19a1fc5
Mauricio Mutte <litiobr@gmail.com>
2019-10-09 17:52:41 -0300

    feat: database connect

--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Database
+mongoose.connect(
+  'mongodb://localhost:27017/node-login', 
+  { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true,
+  },
+);
 
 app.listen(3030);
