4e709a076f9db70a6d9394046598fb26223e1ea0
Mauricio Mutte <litiobr@gmail.com>
2019-10-09 20:37:39 -0300

    feat: add routes

--- packages/backend/src/routes.js
+++ packages/backend/src/routes.js
@@ -1,4 +1,19 @@
 const express = require('express');
 const routes = express.Router();
 
+const auth = require('./middleware/auth');
+const Register = require('./controllers/Register');
+const Login = require('./controllers/Login');
+
+//Home
+routes.get('/', auth, (req, res) => {
+  res.send('HOME!')
+});
+
+//Register
+routes.post('/register', Register);
+
+// Login
+routes.post('/login', Login);
+
 module.exports = routes;
