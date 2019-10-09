bd576bc17a7454eddd0f0abdc83df7c714f8b866
Mauricio Mutte <litiobr@gmail.com>
2019-10-09 20:37:00 -0300

    feat: add auth middleware

---
+++ packages/backend/src/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = function(req, res, next) {
+  const token = req.headers['auth-token'] || req.headers['authorization'];
+
+  if(!token) {
+    return res.status(401).send('Access denied! No token provided.');
+  }
+
+  try {
+    const verified = jwt.verify(token, process.env.SECRET_KEY);
+    req.user = verified;
+
+    next();
+  } catch (err) {
+    res.status(400).send('Invalid token.');
+  }
+}
