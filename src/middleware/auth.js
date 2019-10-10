1d28d52f2182ba77cfa56a931f6c8f7edb5ab84e
Mauricio Mutte <litiobr@gmail.com>
2019-10-10 16:14:36 -0300

    fix: fix simple if

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,9 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next) {
   const token = req.headers['auth-token'] || req.headers['authorization'];
 
-  if(!token) {
-    return res.status(401).send('Access denied! No token provided.');
-  }
+  if(!token) return res.status(401).send('Access denied! No token provided.');
 
   try {
     const verified = jwt.verify(token, process.env.SECRET_KEY);
